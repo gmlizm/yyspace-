db8d6960c9cebb41d95700784c4a6a955d3ea036
gmlizm <gmlizm@gmail.com>
2018-01-16 04:20:20 -0500

    modify

--- bin/iss.sh
+++ bin/iss.sh
@@ -38,8 +38,8 @@ TAR_NAME=$(ls $APP_DIR/ext|sort -r|grep -m 1 -E "$SERVICE_NAME.*-[0-9\.]+(-.*)?\
 PID_FILE=$IPHARM_HOME/bin/.tmp/${SERVICE_NAME}.pid
 
 # parse used config
-SERVER_PORT=`sed '/dubbo.protocol.port/!d;s/.*=//' $APP_DIR/etc/$DUBBO_PROPERTIES_FILE | tr -d '\r'`
-JVM_OPTION=`sed '/JVM_OPTION/!d;s/=//;s/JVM_OPTION//' $APP_DIR/etc/$DUBBO_PROPERTIES_FILE | tr -d '\r'`
+SERVER_PORT=`sed '/^dubbo.protocol.port/!d;s/.*=//' $APP_DIR/etc/$DUBBO_PROPERTIES_FILE | tr -d '\r'`
+JVM_OPTION=`sed '/^JVM_OPTION/!d;s/=//;s/JVM_OPTION//' $APP_DIR/etc/$DUBBO_PROPERTIES_FILE | tr -d '\r'`
 if [ -z "$JVM_OPTION" ];then JVM_OPTION="-server -Xms512m -Xmx512m -Xmn128m -XX:+UseParallelGC -XX:MetaspaceSize=256M -XX:MaxMetaspaceSize=256M"; fi
 JVM_OPTION=$JVM_OPTION' -agentpath:'$IPHARM_HOME'/bin/hook/libipharmacare_hook.so'
 
---
+++ xmemo/db_exp.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+DB_HOST=10.1.1.186
+DB_USER=root
+DB_PWD=root
+SQLDIR=dbscript
+
+mkdir -p ${SQLDIR}
+mysqldump -h ${DB_HOST} -u ${DB_USER} -p ${DB_PWD} ipharmacare_gy --no-data > ${SQLDIR}/create_gy.sql
+sed -ri 's/AUTO_INCREMENT=[0-9]*\ //g' ${SQLDIR}/create_gy.sql
+
+mysqldump -h ${DB_HOST} -u ${DB_USER} -p ${DB_PWD} ipharmacare_dp --no-data > ${SQLDIR}/create_dp.sql
+sed -ri 's/AUTO_INCREMENT=[0-9]*\ //g' ${SQLDIR}/create_dp.sql
+
+mysqldump -h ${DB_HOST} -u ${DB_USER} -p ${DB_PWD} ipharmacare_report --no-data > ${SQLDIR}/create_report.sql
+sed -ri 's/AUTO_INCREMENT=[0-9]*\ //g' ${SQLDIR}/create_report.sql
+
+mysqldump -h ${DB_HOST} -u ${DB_USER} -p ${DB_PWD} ipharmacare_upload --no-data > ${SQLDIR}/create_upload.sql
+sed -ri 's/AUTO_INCREMENT=[0-9]*\ //g' ${SQLDIR}/create_upload.sql
+
+
+mysqldump -h ${DB_HOST} -u ${DB_USER} -p ${DB_PWD} ipharmacare_report --no-create-info --replace --tables report report_item report_item_calc report_item_relation report_script_config > ${SQLDIR}/data_report.sql
+
+mysqldump -h ${DB_HOST} -u ${DB_USER} -p ${DB_PWD} ipharmacare_upload --no-create-info --replace --tables upload_dt_name upload_rule upload_ver_map > ${SQLDIR}/data_upload.sql
+
